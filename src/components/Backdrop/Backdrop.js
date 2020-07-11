a23b4c38898e13556fdae23ee8ef2df01c80aa69
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-07-11 23:18:35 +0530

    Added CSS Properties

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './Backdrop.css';
 
 const backdrop = (props) => {
+    //Making it dynamic using props
     const cssClasses = ['Backdrop', props.show ? 'BackdropOpen' : 'BackdropClose'];
     return (<div className={cssClasses.join(' ')}></div>)
 }
